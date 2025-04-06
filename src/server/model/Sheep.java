ffa95315d28479343882d03c70c870f2ca0b5554
Jimmy <salah.djamel2004@gmail.com>
2025-04-06 19:24:51 +0200

    exit sliding menu implemented

--- src/server/model/Sheep.java
+++ src/server/model/Sheep.java
@@ -3,10 +3,10 @@ package server.model;
 import javax.swing.text.Position;
 import java.awt.*;
 
-public class Sheap extends Livestock{
+public class Sheep extends Livestock{
 
 
-    public Sheap(float health, Point position, int campId, int age) {
+    public Sheep(float health, Point position, int campId, int age) {
         super(health, position, campId, age);
     }
 
