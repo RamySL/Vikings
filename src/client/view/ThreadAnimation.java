67d87114a4e4ecba2d3981413813c46e816959d3
ismael.wane <ismael.wane@universite-paris-saclay.fr>
2025-04-07 15:27:07 +0200

    test

---
+++ src/client/view/ThreadAnimation.java
@@ -0,0 +1,9 @@
+package client.view;
+
+public class ThreadAnimation extends Thread{
+    private static img0 =
+    private static img1 =
+    private static img2  =
+    private static img3 =
+    private static img4 =
+}
